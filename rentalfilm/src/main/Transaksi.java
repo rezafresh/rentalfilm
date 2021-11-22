840514396d426f40758a6d26340dd24f108b165a
RezaFresh <31729829+rezafresh@users.noreply.github.com>
2021-11-22 11:20:04 +0700

    Add files via upload

--- rentalfilm/src/main/Transaksi.java
+++ rentalfilm/src/main/Transaksi.java
@@ -5,17 +5,69 @@
  */
 package main;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author User
  */
 public class Transaksi extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Transaksi
-     */
+
+    Connection koneksi;
+    
     public Transaksi() {
         initComponents();
+        koneksi = DatabaseConnection.getKoneksi("localhost", "3306", "root", "", "rentalfilm");
+        showData();
+        showComboBox();
+    }
+    
+    DefaultTableModel dtm;
+    
+    void showData(){
+        String[] kolom = {"NO", "FILM", "HARGA SEWA"};
+        
+        dtm = new DefaultTableModel(null, kolom);
+        try{
+            Statement stmt = koneksi.createStatement();
+            String query = "SELECT * FROM film";
+            ResultSet rs = stmt.executeQuery(query);
+            int no = 1;
+            
+            while (rs.next()){
+                String nama = rs.getString("nama");
+                String harga = rs.getString("hargasewasehari");
+                
+                dtm.addRow(new String[]{no+"",nama,harga});
+                no++;
+            }
+        } catch (SQLException ex){
+            ex.printStackTrace();
+            jOptionPane.showMessageDialog(null, "Terjadi Kesalahan di Query");
+        }
+        
+        tabelfilm.setModel(dtm);
+    }
+    
+    //updatecombobox
+    private void showComboBox(){
+        try {
+            Statement stmt = koneksi.createStatement();
+            String sql = "SELECT * from kasir";
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()) {
+                String nama = rs.getString("nama");
+                comboBoxKasir.addItem(nama);
+            }
+        } catch (Exception e) {
+            
+        }
     }
 
     /**
@@ -27,21 +79,100 @@ public class Transaksi extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tabelfilm = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        comboBoxKasir = new javax.swing.JComboBox();
+        lblKasir = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        lblKasir1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        fieldPembeli = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        tabelfilm.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        tabelfilm.setName(""); // NOI18N
+        jScrollPane1.setViewportView(tabelfilm);
+
+        jLabel1.setText("Rental Film");
+
+        comboBoxKasir.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Pilih Kasir" }));
+        comboBoxKasir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                comboBoxKasirActionPerformed(evt);
+            }
+        });
+
+        lblKasir.setText("Kasir");
+
+        jLabel2.setText(":");
+
+        lblKasir1.setText("Pembeli");
+
+        jLabel3.setText(":");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(15, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(152, 152, 152)
+                        .addComponent(jLabel1))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(lblKasir, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblKasir1))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 7, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 7, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(45, 45, 45)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(comboBoxKasir, 0, 149, Short.MAX_VALUE)
+                            .addComponent(fieldPembeli))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(19, 19, 19)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(comboBoxKasir, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblKasir)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblKasir1)
+                    .addComponent(jLabel3)
+                    .addComponent(fieldPembeli, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 25, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void comboBoxKasirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboBoxKasirActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_comboBoxKasirActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +210,14 @@ public class Transaksi extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox comboBoxKasir;
+    private javax.swing.JTextField fieldPembeli;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblKasir;
+    private javax.swing.JLabel lblKasir1;
+    private javax.swing.JTable tabelfilm;
     // End of variables declaration//GEN-END:variables
 }
